e3cb30a540b582acbb3f921b70ef2cb9943323ef
Stacey Brogden <toad@brothertoad.net>
2023-11-05 12:52:29 -0800

    Improve styling of EZMask table.

--- pre.sh
+++ pre.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-awk -F, '{ print "<tr><td>" $1 "</td><td>" $2 "</td><td>" $3 "</td><td>" $4 "</td></tr>"}' lists/EZMasks-total.csv > lists/EZMasks-table.html
+awk -F, '{ print "<tr><td class=\042tright\042>" $1 "</td><td class=\042tleft\042>" $2 "</td><td>" $3 "</td><td>" $4 "</td></tr>"}' lists/EZMasks-total.csv > lists/EZMasks-table.html
